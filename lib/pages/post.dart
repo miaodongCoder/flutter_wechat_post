e2c3cf72a2e9419eec3d4d72b8e2ee175b8eb831
miaod <miaod@mail.open.com.cn>
2023-03-30 18:42:53 +0800

    提交相册选取与添加按钮的布局约束;

--- lib/pages/post.dart
+++ lib/pages/post.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_wechat_post/utils/config.dart';
 import 'package:wechat_assets_picker/wechat_assets_picker.dart';
 
 class PostEditPage extends StatefulWidget {
@@ -9,6 +10,8 @@ class PostEditPage extends StatefulWidget {
 }
 
 class _PostEditPageState extends State<PostEditPage> {
+  // 选择的图片数组:
+  List<AssetEntity> selectedAssets = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,18 +23,81 @@ class _PostEditPageState extends State<PostEditPage> {
   }
 
   Widget _mainView() {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          ElevatedButton(
-              onPressed: () async {
-                final List<AssetEntity>? result =
-                    await AssetPicker.pickAssets(context);
-                print("选取的图片数量: ${result?.length}");
-              },
-              child: const Text("选取图片"))
-        ],
+    return Column(
+      children: [
+        // 九宫格图片列表:
+        _buildPhotoList(),
+      ],
+    );
+  }
+
+  // 构建图片列表:
+  Widget _buildPhotoList() {
+    return Padding(
+      padding: const EdgeInsets.all(spacing),
+      child: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          final double width = (constraints.maxWidth - spacing * 2) / 3;
+          return Wrap(
+            spacing: spacing,
+            runSpacing: spacing,
+            children: [
+              // 1.图片:
+              for (AssetEntity asset in selectedAssets)
+                _buildPhotoItems(asset, width),
+              // 2.按钮:
+              if (selectedAssets.length < maxAssets)
+                _buildAddButton(context, width),
+            ],
+          );
+        },
+      ),
+    );
+  }
+
+  // 图片项:
+  Container _buildPhotoItems(AssetEntity asset, double width) {
+    return Container(
+      clipBehavior: Clip.antiAlias,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(3),
+      ),
+      child: AssetEntityImage(
+        asset,
+        width: width,
+        height: width,
+        fit: BoxFit.cover,
+        isOriginal: false,
+      ),
+    );
+  }
+
+  // 添加图片按钮:
+  GestureDetector _buildAddButton(BuildContext context, double width) {
+    return GestureDetector(
+      onTap: () async {
+        final List<AssetEntity>? assets = await AssetPicker.pickAssets(
+          context,
+          pickerConfig: AssetPickerConfig(
+            selectedAssets: selectedAssets,
+            maxAssets: maxAssets,
+          ),
+        );
+
+        // 拿到图片:
+        setState(() {
+          selectedAssets = assets ?? [];
+        });
+      },
+      child: Container(
+        color: Colors.black12,
+        width: width,
+        height: width,
+        child: const Icon(
+          Icons.add,
+          size: 48,
+          color: Colors.black45,
+        ),
       ),
     );
   }
---
+++ lib/utils/config.dart
@@ -0,0 +1,3 @@
+const double spacing = 10.0;
+// 图片选取数量最大值:
+const int maxAssets = 6;
